8c939b00c4bffa4feba374b435be5132ee3525cb
Lunke <CMLunke@landolakes.com>
2016-05-08 13:13:11 -0500

    Testing bluetooth pair listener.

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ import gobject
 import subprocess
 
 # ID of the device we care about
-DEV_ID = 'D8_D1_CB_A6_66_DA '
+DEV_ID = 'D8_D1_CB_A6_66_DA'
 
 dbus_loop = DBusGMainLoop()
 bus = dbus.SystemBus(mainloop=dbus_loop)
@@ -15,4 +15,16 @@ man = bus.get_object('org.bluez', '/')
 iface = dbus.Interface(man, 'org.bluez.Manager')
 adapterPath = iface.DefaultAdapter()
 
-headset = bus.get_object('org.bluez', adapterPath + '/dev_' + DEV_ID)
+iphone = bus.get_object('org.bluez', adapterPath + '/dev_' + DEV_ID)
+print (iphone)
+
+def cb(iface=None, mbr=None, path=None):
+	print("iface: " + iface)
+	print("mbr: " + mbr)
+	print("path: " + path)
+
+headset.connect_to_signal("Connected", cb, interface_keyword='iface', member_keyword='mbr', path_keyword='path')
+headset.connect_to_signal("Disconnected", cb, interface_keyword='iface', member_keyword='mbr', path_keyword='path')
+
+loop = gobject.MainLoop()
+loop.run()
